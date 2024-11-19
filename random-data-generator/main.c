2e45323778b7f45bedaf30c551007ab3850cb0a2
Jacob Thompson <jacobalthompson@gmail.com>
2024-11-19 11:18:44 -0800

    Initial Commit

---
+++ random-data-generator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  random-data-generator
+//
+//  Created by Jacob Thompson on 11/19/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
